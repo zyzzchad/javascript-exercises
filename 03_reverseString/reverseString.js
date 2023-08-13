6b0582af3f191fa8a161019a32211666aae4340b
Alish Gautam <alishgautam890@gmail.com>
2023-08-13 21:51:09 +0545

    Here I've completed removeString,removeFromArray and sumAll from the challenges of TOP

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(userInp) {
+    userInp=userInp.split("")
+     userInp.reverse()
+     return userInp.join("")
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    let sum=0
+    if(start<0||end<0||typeof start!="number" || typeof end!="number"){
+        return "ERROR"
+    }
+    else if(start<end){
+        for(let i=start;i<=end;i++){
+            sum+=i
+        }
+        return sum
+    }
+    else if(start>end){
+        for(let i=end;i<=start;i++){
+            sum+=i
+        }
+        return sum
+    }
+   
 
+ 
 };
 
 // Do not edit below this line
