b6b8d900b9f068e2bfcb137e4db28d94614dafbe
Alish Gautam <alishgautam890@gmail.com>
2023-08-14 07:05:46 +0545

    Leap year finder

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if((year%4==0 && year%100!=0)||year%400==0){
+        return true
+    }
+    else if((year%100==0 && year%400!=0)||year%2!=0){
+        return false
+    }
 };
 
 // Do not edit below this line
