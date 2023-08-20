bb6e5ab4ca61c613188179b45508b210c6ce835e
Alish Gautam <alishgautam890@gmail.com>
2023-08-20 06:54:37 +0545

    calculator and palindrome done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total,item)=>total+item,0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) { 
+  return arr.reduce((total,item)=>total*item)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	 return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let product=1
+	if (n==0) return 1
+  else{
+    for (let i=n;i>0;i--){
+       product*=i
+    }
+    return product
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return processedString.split("").reverse().join("") == processedString;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
