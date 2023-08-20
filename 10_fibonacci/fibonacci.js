a52cbde7b7519d1759519cadfd0866f85cb22ae1
Alish Gautam <alishgautam890@gmail.com>
2023-08-20 08:37:23 +0545

    fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<0){
+        return "OOPS"
+    }
+    Number(num)
+    let firstnum=0;
+    let secondnum=1;
+    let fib=[secondnum]
+    let sum=0
+    
+    for(let i=1;i<=num;i++){
+        sum=firstnum+secondnum
+        fib.push(sum)
+        firstnum=secondnum
+        secondnum=sum
+    }
+    return fib[num-1]
+   
 };
 
 // Do not edit below this line
