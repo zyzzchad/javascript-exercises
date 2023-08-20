a0698b8550c377909f0ea6b7118338944c6bd129
Alish Gautam <alishgautam890@gmail.com>
2023-08-20 21:24:42 +0545

    Completed second last lesson of foundations of The Odin Project

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+    let titles=books.map((book)=>{
+        return book.title
+    })
+    return titles
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+   return people.reduce((oldestPeople,currentPeople)=>{
+    let oldestAge=stillAlive(oldestPeople.yearOfDeath,oldestPeople.yearOfBirth)
+    let currenAge=stillAlive(currentPeople.yearOfDeath,currentPeople.yearOfBirth)
+    if(oldestAge>currenAge){
+        return oldestPeople
+       }
+       else{
+        return currentPeople
+       }
+   })
+   
 };
+function stillAlive(death,birth){
+    if(!death){
+         death=new Date().getFullYear()
+    }
+    return death-birth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
